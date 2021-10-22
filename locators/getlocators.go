b490cb4a881f3feac40319f7b8df8048bb47cc96
derganc <derganc@iskratel.si>
2021-10-22 11:06:16 +0200

    locators.Get

--- locators/getlocators.go
+++ locators/getlocators.go
@@ -1,14 +1,14 @@
-package api
+package locators
 
 import (
 	"github.com/s51ds/qthdb/db"
 	"github.com/s51ds/qthdb/row"
 )
 
-// Locators returns all locators for callSign sorted by time
+// Get returns all locators for callSign sorted by time
 // IMPORTANT: app which use this module, must load DB before.
 // e.g. db.Open("../app/db.gob")
-func Locators(callSign string) (resp []row.QueryResponse) {
+func Get(callSign string) (resp []row.QueryResponse) {
 	rec, _ := db.Get(callSign)
 	return rec.Locators.SortedByTime()
 }
